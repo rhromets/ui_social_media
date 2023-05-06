8e4f23251990be00265e2dfc4e2327095337b271
Roman Khromets <rhromets@gmail.com>
2023-05-06 13:11:43 +0300

    Add Drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_ui/widgets/custom_drawer.dart';
 import 'package:social_media_ui/widgets/following_users.dart';
 import 'package:social_media_ui/widgets/posts_carousel.dart';
 import 'package:social_media_ui/data/data.dart' as data;
@@ -55,6 +56,7 @@ class _HomeScreenState extends State<HomeScreen>
           ],
         ),
       ),
+      drawer: const CustomDrawer(),
       body: ListView(
         children: [
           const FollowingUsers(),
